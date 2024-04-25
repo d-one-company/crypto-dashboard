2c294acc61265189dc28182d9b55f11008519fef
vukasin <vukasin.mladenovic@d-one.design>
2024-04-25 13:59:12 +0200

    feat: add t3 env

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+import _jiti from 'jiti';
+import { fileURLToPath } from 'url';
+
+const jiti = _jiti(fileURLToPath(import.meta.url));
+
+jiti('./env.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
