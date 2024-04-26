280f432852164281ed011de62caa81dd8ad3444c
blink-dev <nikola.mirjanic@d-one.design>
2024-04-26 13:06:39 +0200

    feat(dashboard): design

---
+++ app/dashboard/RecentTransaction.tsx
@@ -0,0 +1,57 @@
+import DummyChart, { data } from '@/components/DummyChart';
+import { cn } from '@/lib/utils';
+import { cloneElement } from 'react';
+
+type Props = {
+  icon: JSX.Element;
+  label: string;
+  symbol: string;
+  marketCap: number;
+  marketCapColor: string;
+  chance: number;
+};
+
+const RecentTransaction = ({
+  icon,
+  label,
+  symbol,
+  marketCap,
+  marketCapColor,
+  chance,
+}: Props) => {
+  return (
+    <div className="grid grid-cols-5 items-center gap-5 rounded-xl bg-card-dark p-5 shadow-card">
+      <div className="flex items-center gap-6">
+        {cloneElement(icon, { width: 26, height: 26 })}
+        <div className="flex flex-col gap-0.5 text-xs">
+          <p className="text-white">{label}</p>
+          <p className="text-foreground-dark">{symbol}</p>
+        </div>
+      </div>
+      <div className="flex flex-col gap-0.5 text-xs">
+        <p className="text-white">Market Cap</p>
+        <p className={cn('text-red-500', marketCapColor)}>
+          {`$${Intl.NumberFormat('en', { currency: 'USD' }).format(marketCap)}`}
+        </p>
+      </div>
+      <div className="flex flex-col gap-0.5 text-xs">
+        <p className="text-white">24h chance</p>
+        <p className="text-green-500">
+          +{Intl.NumberFormat('en').format(chance)}%
+        </p>
+      </div>
+      <div className="flex flex-col gap-0.5 text-xs">
+        <DummyChart data={data} curve="basis" />
+      </div>
+      <button className="ml-auto w-fit rounded-xl bg-white px-4 py-2 text-black shadow-button">
+        Trade
+      </button>
+    </div>
+  );
+};
+
+RecentTransaction.defaultProps = {
+  marketCapColor: 'text-red-500',
+};
+
+export default RecentTransaction;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,126 @@
+import {
+  Bitcoin,
+  DotsHorizontal,
+  Ethereum,
+  Tether,
+  Trending,
+} from '@/components/icons';
+import RecentTransaction from './RecentTransaction';
+
+const Dashboard = () => {
+  return (
+    <div className="flex h-screen flex-col gap-5 bg-background p-5">
+      <h1 className="mb-4 text-lg">Hi, Alex</h1>
+      <div className="flex items-stretch gap-4">
+        <div className="min-w-[400px] rounded-xl bg-card-dark p-5 shadow-card">
+          <div className="flex items-center justify-between">
+            <p className="text-sm">Current Balance</p>
+            <button>
+              <DotsHorizontal />
+            </button>
+          </div>
+          <p className="mt-4 text-3xl">
+            <span className="text-foreground-dark">$</span>
+            <span>{Intl.NumberFormat('en').format(21432.15)}</span>
+          </p>
+          <div className="mt-2 flex items-center gap-1">
+            <div className="inline-block rounded border border-green-700 p-0.5">
+              <Trending className="text-green-700" width={10} height={10} />
+            </div>
+            <span className="text-xs text-green-700">12% last month</span>
+          </div>
+          <div className="mt-4 flex items-center gap-2">
+            <button className="rounded-xl bg-white px-3.5 py-2 text-black shadow-button">
+              Balance
+            </button>
+            <button className="px-3.5 py-2">Quick Invest</button>
+            <button className="px-3.5 py-2">Actives</button>
+          </div>
+        </div>
+
+        <div className="min-w-[400px] rounded-xl bg-card-dark p-5 shadow-card">
+          <div className="flex items-center justify-between">
+            <p className="text-sm">Recent Transactions</p>
+            <button>
+              <DotsHorizontal />
+            </button>
+          </div>
+          <div className="mt-4 flex flex-col gap-2 text-xs">
+            <div className="flex items-center gap-3">
+              <Bitcoin width={26} height={26} />
+              <div className="grow">
+                <p className="flex justify-between gap-2 text-white">
+                  <span>Bitcoin (BTC)</span>
+                  <span>$5,454</span>
+                </p>
+                <p className="flex justify-between gap-2 text-foreground-dark">
+                  <span>Buy</span>
+                  <span>Today, 12:41</span>
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <Ethereum width={26} height={26} />
+              <div className="grow">
+                <p className="flex justify-between gap-2 text-white">
+                  <span>Ethereum (ETC)</span>
+                  <span>+ 31,414 ETH</span>
+                </p>
+                <p className="flex justify-between gap-2 text-foreground-dark">
+                  <span>Received</span>
+                  <span>Today, 11:49</span>
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center gap-3">
+              <Tether width={26} height={26} />
+              <div className="grow">
+                <p className="flex justify-between gap-2 text-white">
+                  <span>Tether (USDT)</span>
+                  <span>+ 0,4144 USDT</span>
+                </p>
+                <p className="flex justify-between gap-2 text-foreground-dark">
+                  <span>Received</span>
+                  <span>Today, 06:12</span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex items-center justify-between gap-2">
+        <span className="text-lg">Recent Transactions</span>
+        <button className="font-light text-foreground-dark">
+          See all coins
+        </button>
+      </div>
+      <RecentTransaction
+        icon={<Bitcoin />}
+        label="Bitcoin"
+        symbol="BTC"
+        chance={1.41}
+        marketCap={24413.23}
+        marketCapColor="text-red-500"
+      />
+      <RecentTransaction
+        icon={<Ethereum />}
+        label="Ethereum"
+        symbol="ETH"
+        chance={1.41}
+        marketCap={1253.23}
+        marketCapColor="text-blue-500"
+      />
+      <RecentTransaction
+        icon={<Tether />}
+        label="Tether"
+        symbol="USDT"
+        chance={1.41}
+        marketCap={314224.12}
+        marketCapColor="text-green-500"
+      />
+    </div>
+  );
+};
+
+export default Dashboard;
