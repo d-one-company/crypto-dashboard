7e980ccbc4afb86de92c82bbb9cf5365d6ffa89d
vukasin <vukasin.mladenovic@d-one.design>
2024-05-10 19:09:23 +0200

    fix: remove OKX env variables from t3

--- env.ts
+++ env.ts
@@ -11,15 +11,9 @@ export const env = createEnv({
   },
   shared: {
     //
-    OKX_API_KEY: z.string(),
-    OKX_API_SECRET: z.string(),
-    OKX_API_PASSPHRASE: z.string(),
   },
   runtimeEnv: {
     POSTGRES_URL: process.env.POSTGRES_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    OKX_API_KEY: process.env.NEXT_PUBLIC_OKX_API_KEY,
-    OKX_API_SECRET: process.env.NEXT_PUBLIC_OKX_API_SECRET,
-    OKX_API_PASSPHRASE: process.env.NEXT_PUBLIC_OKX_API_PASSPHRASE,
   },
 });
