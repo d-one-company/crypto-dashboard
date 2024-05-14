9eb75dd6abb5edec6f3d969bf40a3052eaed8f80
banidja <branislav.jaksic@d-one.design>
2024-05-14 12:15:56 +0200

    fix: remove info containers from market page

--- components/Market/Market.tsx
+++ components/Market/Market.tsx
@@ -2,41 +2,15 @@
 
 import { useCoinsContext } from '@/providers/crypto/CoinsProvider';
 import AssetsTable from '../Assets/AssetsTable';
-import InfoContainer from './InfoContainer';
-
-const formatNumber = (number: number): string => {
-  const formattedNumber = Number.parseFloat(number.toString()).toFixed(2);
-  return parseFloat(formattedNumber).toLocaleString('en-US');
-};
 
 const Market = () => {
   const {
     coinsStore: { coins },
-    isLoading,
   } = useCoinsContext();
-
-  if (isLoading) null;
-
-  const totalMarketCap = coins.reduce((acc, coin) => {
-    const marketCapValue = parseFloat(String(coin.marketCapUsd));
-    return acc + (marketCapValue || 0);
-  }, 0);
-
-  const totalVolume24Hr = coins.reduce((acc, coin) => acc + (coin.volumeUsd24Hr || 0), 0);
-
-  const btc = coins.find(coin => coin.id === 'bitcoin');
-  const btcMarketCap = btc ? btc.marketCapUsd : 0;
-  const btcDominance = (btcMarketCap / totalMarketCap) * 100;
 
   return (
     <div className="flex w-full flex-col gap-10">
       <h1 className="mt-20 self-center text-2xl font-bold">Today&apos;s Crypto Prices by Market Cap</h1>
-      <div className="flex w-full justify-between gap-10">
-        {/* todo: figure out how to calcualte the percentages */}
-        <InfoContainer text="Market Cap" percentage={12} value={`$${formatNumber(totalMarketCap)}`} />
-        <InfoContainer text="Volume 24h" percentage={12} value={`$${formatNumber(totalVolume24Hr)}`} />
-        <InfoContainer text="BTC Dominance" percentage={12} value={`${formatNumber(btcDominance)}%`} />
-      </div>
       <AssetsTable coins={coins} />
     </div>
   );
