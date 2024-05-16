89a3aa9b12b8a99e2e4697974a451b3de243cf5f
Branislav Jaksic <branislav.jaksic@d-one.design>
2024-05-16 17:42:54 +0200

    feat: add arrows to indicate price change (#15)

---
+++ app/dashboard/RecentTransactionPrice.tsx
@@ -0,0 +1,30 @@
+import { ArrowDownIndicator, ArrowUpIndicator } from '@/components/ArrowIndicators';
+import { Coin } from '@/providers/crypto/useCoinsStore';
+import { useEffect, useState } from 'react';
+type Props = { coin: Coin };
+
+const RecentTransactionPrice = ({ coin }: Props) => {
+  const [arrow, setArrow] = useState(<ArrowUpIndicator />);
+  const [prevPrice, setPrevPrice] = useState(coin.priceUsd);
+
+  useEffect(() => {
+    if (coin.priceUsd !== prevPrice) {
+      const newArrow = coin.priceUsd > prevPrice ? <ArrowUpIndicator /> : <ArrowDownIndicator />;
+      setArrow(newArrow);
+
+      setPrevPrice(coin.priceUsd);
+    }
+  }, [coin.priceUsd, prevPrice]);
+
+  return (
+    <div className="flex items-center gap-2">
+      {arrow}
+      <div className="flex flex-col justify-center gap-0.5 text-xs">
+        <span className="text-white">Price</span>
+        <span className="flex items-center gap-2 text-foreground-dark">{Intl.NumberFormat('en', { style: 'currency', currency: 'USD' }).format(coin.priceUsd)}</span>
+      </div>
+    </div>
+  );
+};
+
+export default RecentTransactionPrice;
---
+++ components/Dashboard/CryptoPrice.tsx
@@ -0,0 +1,28 @@
+import { Coin } from '@/providers/crypto/useCoinsStore';
+import { useEffect, useState } from 'react';
+import { ArrowDownIndicator, ArrowUpIndicator } from '../ArrowIndicators';
+
+type Props = { coin: Coin };
+
+const CryptoPrice = ({ coin }: Props) => {
+  const [arrow, setArrow] = useState(<ArrowUpIndicator />);
+  const [prevPrice, setPrevPrice] = useState(coin.priceUsd);
+
+  useEffect(() => {
+    if (coin.priceUsd !== prevPrice) {
+      const newArrow = coin.priceUsd > prevPrice ? <ArrowUpIndicator /> : <ArrowDownIndicator />;
+      setArrow(newArrow);
+
+      setPrevPrice(coin.priceUsd);
+    }
+  }, [coin.priceUsd, prevPrice]);
+
+  return (
+    <div className="flex items-center gap-2">
+      {new Intl.NumberFormat('en').format(coin?.priceUsd ?? 0)}
+      {arrow}
+    </div>
+  );
+};
+
+export default CryptoPrice;
