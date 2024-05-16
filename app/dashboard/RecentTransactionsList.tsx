225b59b6ffe74677689d75de989937545a07a90c
vukasin <vukasin.mladenovic@d-one.design>
2024-05-16 17:43:17 +0200

    feat: add store instances for assets & transactions

--- app/dashboard/RecentTransactionsList.tsx
+++ app/dashboard/RecentTransactionsList.tsx
@@ -6,20 +6,16 @@ import RecentTransaction from './RecentTransaction';
 
 const RecentTransactionsList = () => {
   const {
-    coinsStore: { coins },
+    coinsStore: { recentCoins },
   } = useCoinsContext();
 
-  const threeCoinsIds = ['bitcoin', 'solana', 'dogecoin'];
   return (
     <Observer>
       {() => (
         <>
-          {coins
-            .filter(coin => threeCoinsIds.includes(coin.id))
-            .slice(0, 3)
-            .map((coin, idx) => (
-              <RecentTransaction key={coin?.id || idx} coin={coin} />
-            ))}
+          {recentCoins.map((coin, idx) => (
+            <RecentTransaction key={coin?.id || idx} coin={coin} />
+          ))}
         </>
       )}
     </Observer>
