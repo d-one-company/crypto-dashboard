7268c2c7d3fcc04f7ac681db02cec9d848e79d12
blink-dev <nikola.mirjanic@d-one.design>
2024-05-30 18:31:52 +0200

    fix: restart socket after close

--- apps/trades/src/index.ts
+++ apps/trades/src/index.ts
@@ -5,56 +5,71 @@ import { WebSocket } from "ws";
 
 const trades: TradesData[] = [];
 
-const ws = new WebSocket(
-  `wss://ws.coincap.io/trades/huobi?base=${BASE}&quote=${QUOTE}`,
-);
+let ws: WebSocket;
 
-ws.onmessage = (event) => {
-  const trade = JSON.parse(event.data.toString()) as Trade;
-  const date = dayjs(trade.timestamp);
-  const interval = date.format("YYYY-MM-DD HH:mm");
+const socketUrl = `wss://ws.coincap.io/trades/huobi?base=${BASE}&quote=${QUOTE}`;
 
-  const index = trades.findIndex((t) => t.interval === interval);
-  if (index === -1) {
-    if (trades.length >= MAX_BARS) trades.shift();
-    trades.push({
-      interval,
-      open: trade.price,
-      close: trade.price,
-      buy: {
-        count: 0,
-        volume: 0,
-        price: 0,
-        priceUsd: 0,
-      },
-      sell: {
-        count: 0,
-        volume: 0,
-        price: 0,
-        priceUsd: 0,
-      },
-    });
+const startSocket = () => {
+  ws = new WebSocket(socketUrl);
+  ws.onclose = () => setTimeout(() => startSocket(), 5000);
+};
+
+const readSocket = () => {
+  if (!ws) {
+    console.error("Socket not initialized");
+    return;
   }
 
-  const tradesData = trades[index];
-  if (tradesData) {
-    if (trade.direction === "buy") {
-      tradesData.buy.count += 1;
-      tradesData.buy.volume += trade.volume;
-      tradesData.buy.price += trade.price;
-      if (trade.priceUsd) tradesData.buy.priceUsd += trade.priceUsd;
-    } else {
-      tradesData.sell.count += 1;
-      tradesData.sell.volume += trade.volume;
-      tradesData.sell.price += trade.price;
-      if (trade.priceUsd) tradesData.sell.priceUsd += trade.priceUsd;
+  ws.onmessage = (event) => {
+    const trade = JSON.parse(event.data.toString()) as Trade;
+    const date = dayjs(trade.timestamp);
+    const interval = date.format("YYYY-MM-DD HH:mm");
+
+    const index = trades.findIndex((t) => t.interval === interval);
+    if (index === -1) {
+      if (trades.length >= MAX_BARS) trades.shift();
+      trades.push({
+        interval,
+        open: trade.price,
+        close: trade.price,
+        buy: {
+          count: 0,
+          volume: 0,
+          price: 0,
+          priceUsd: 0,
+        },
+        sell: {
+          count: 0,
+          volume: 0,
+          price: 0,
+          priceUsd: 0,
+        },
+      });
     }
 
-    tradesData.close = trade.price;
-  }
+    const tradesData = trades[index];
+    if (tradesData) {
+      if (trade.direction === "buy") {
+        tradesData.buy.count += 1;
+        tradesData.buy.volume += trade.volume;
+        tradesData.buy.price += trade.price;
+        if (trade.priceUsd) tradesData.buy.priceUsd += trade.priceUsd;
+      } else {
+        tradesData.sell.count += 1;
+        tradesData.sell.volume += trade.volume;
+        tradesData.sell.price += trade.price;
+        if (trade.priceUsd) tradesData.sell.priceUsd += trade.priceUsd;
+      }
 
-  trades[index] = tradesData;
+      tradesData.close = trade.price;
+    }
+
+    trades[index] = tradesData;
+  };
 };
+
+startSocket();
+readSocket();
 
 const app = express();
 app.use(express.json());
